d2efa1719d737489960201434be7938a62039cda
acubefoundation <abdurahimamin987@gamil.com>
2023-08-05 08:05:35 +0300

    question and answer page finished

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -21,8 +21,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const loginRes = await axios.post(`${process.env.REACT_APP}/api/users/login`
-      ,
+      const loginRes = await axios.post(`http://localhost:4500/api/users/login`,
         {
           email: form.email,
           password: form.password,
@@ -44,7 +43,6 @@ const Login = () => {
     }
   };
   // console.log(userData.user.id)
-  console.log(process.env.REACT_APP)
 
   useEffect(() => {
     if (userData.user) navigate("/");
